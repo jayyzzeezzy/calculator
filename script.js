b1923527384354960c5931fe623b34f5850124e6
James Zhou <jam9es@gmail.com>
2023-09-02 20:36:00 -0700

    Edit existing functions & add logic for equal key

--- script.js
+++ script.js
@@ -21,10 +21,12 @@ let operator = "+";
 let operationIsSet = false;
 
 function operate(a, operator, b) {
+    a = Number(a);
+    b= Number(b);
     if (operator == "+") return add(a, b);
     if (operator == "-") return subtract(a, b);
-    if (operator == "*") return multiply(a, b);
-    if (operator == "/") return divide(a, b);
+    if (operator == "×") return multiply(a, b);
+    if (operator == "÷") return divide(a, b);
 };
 
 // for click events and only numbers
@@ -52,8 +54,16 @@ operatorBtn.forEach(
     (button) => button.addEventListener('click', () => setOperation(screen.textContent, button.textContent))
 );
 
-function setOperation(number, operator) {
+function setOperation(number, sign) {
     firstNumb = number;
-    operator = operator;
+    operator = sign;
     operationIsSet = true;
+};
+
+const equalBtn = document.querySelector('#equalBtn');
+equalBtn.onclick = () => showResult();
+
+function showResult() {
+    secondNumb = screen.textContent;
+    screen.textContent = operate(firstNumb, operator, secondNumb);
 };
